62aaedbee1c140c1cfccf713eed147706eb32050
Elizabeth <liza_vvm@mail.ru>
2021-11-04 00:09:54 +0700

    Настройка начальной страницы

--- SocialNetwork/Startup.cs
+++ SocialNetwork/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,8 +9,19 @@ namespace SocialNetwork
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+
+            // Добавление служб репозиториев
+            //services.AddScoped<IBookRepository, BookRepository>();
+            //services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //services.AddScoped<IOrderRepository, OrderRepository>();
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -17,16 +29,17 @@ namespace SocialNetwork
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
             }
 
+            app.UseStaticFiles();
+            //app.UseSession();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                // Стандартный путь "{controller=Home}/{action=Index}/{id?}"
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
