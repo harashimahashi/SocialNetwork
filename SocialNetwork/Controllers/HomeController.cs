07864403c6f8cc867dddad179c6207ed24479251
Artem Volobuiev <harashimahashi@gmail.com>
2021-11-11 02:19:39 +0200

    Added displaying of the current user

--- SocialNetwork/Controllers/HomeController.cs
+++ SocialNetwork/Controllers/HomeController.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using SocialNetwork.Models.Entities;
 
 namespace SocialNetwork.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private UserManager<ApplicationUser> _userManager { get; set; }
+
+        public HomeController(UserManager<ApplicationUser> userManager) => _userManager = userManager;
+
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            return View(user);
         }
     }
 }
