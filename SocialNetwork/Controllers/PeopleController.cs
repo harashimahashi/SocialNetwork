8a6954a41d9991afbeed7d57d0edbfe0da214dfd
Artem Volobuiev <harashimahashi@gmail.com>
2021-11-15 21:43:46 +0200

    Added user searching

--- SocialNetwork/Controllers/PeopleController.cs
+++ SocialNetwork/Controllers/PeopleController.cs
@@ -15,14 +15,33 @@ namespace SocialNetwork.Controllers
 
         public PeopleController(UserManager<ApplicationUser> userManager) => _userManager = userManager;
 
-        public async Task<IActionResult> Index(string category) {
 
+        public async Task<IActionResult> Index(string category)
+        { 
             var current = await _userManager.FindByNameAsync(User.Identity.Name);
+
             return View(new UserNamesAndCategoryViewModel() { 
                 UserNames = (from u in _userManager.Users.ToList()
                              where ((u.UserName != User.Identity.Name) && (category == "Subscribers" ? u.Subscribed.Contains(current.Id) : true))
-                             select (UserNameViewModel)u).ToList(), 
+                             select (UserNameViewModel)u).Take(6).ToList(), 
                 Category = category ?? "All" 
+            });
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string match, string category)
+        { 
+            var current = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            return View(new UserNamesAndCategoryViewModel()
+            {
+                UserNames = (from u in _userManager.Users.ToList()
+                             where ((u.UserName != User.Identity.Name) && 
+                                    (category == "Subscribers" ? u.Subscribed.Contains(current.Id) : true) &&
+                                    (match is not null ? (u.FirstName + " " + u.LastName).Contains(match) : true))
+                             select (UserNameViewModel)u).Take(6).ToList(),
+                Category = category ?? "All"
             });
         }
     }
